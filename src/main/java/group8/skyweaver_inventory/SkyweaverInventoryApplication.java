21ea13e6fb38f1c1f362a11732166c4c83708103
unknown <ehw4@sfu.ca>
2024-06-18 09:02:08 -0700

    Initial Commit

---
+++ src/main/java/group8/skyweaver_inventory/SkyweaverInventoryApplication.java
@@ -0,0 +1,13 @@
+package group8.skyweaver_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyweaverInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyweaverInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group8/skyweaver_inventory/SkyweaverInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package group8.skyweaver_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyweaverInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
