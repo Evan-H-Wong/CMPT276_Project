1d57d74e8ab393dc0d1ddb2506fb6fd997e06699
Alezis69 <a.bezbocov@gmail.com>
2024-06-25 12:53:03 -0700

    First login branch

---
+++ src/main/java/group8/skyweaver_inventory/controllers/UserController.java
@@ -0,0 +1,76 @@
+package group8.skyweaver_inventory.controllers;
+
+
+
+import group8.skyweaver_inventory.models.User;
+import group8.skyweaver_inventory.models.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public RedirectView redirect() {
+        return new RedirectView("/login.html");
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam Map<String, String> login, Model model, HttpServletRequest request, HttpSession session) {
+        String username = login.get("username");
+        String password = login.get("password");
+        String Id = login.get("Id");
+
+        List<User> users = userRepository.findByUsernameAndPasswordAndId(username, password, Id);
+        // now there is either 1 or 0 users in the list, 1 if there is already a registered user with that username and password
+        if(users.isEmpty()) {
+            return "login.html";
+        } else {
+            User user = users.get(0);
+            request.getSession().setAttribute("sessionUser", user);
+            model.addAttribute("user", user);
+            return "protected.html";
+        }
+    }
+
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
+        User user = (User) session.getAttribute("sessionUser");
+        if (user != null) {
+            return "/login.html";
+        } else {
+            model.addAttribute("user", user);
+            return "/protected.html";
+        }
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam Map<String, String> register, HttpServletResponse response) {
+        String username = register.get("username");
+        String password = register.get("password");
+        String Id = register.get("Id");
+        User user = new User(username, password, Id);
+        userRepository.save(user);
+        response.setStatus(201);
+        return "/login.html";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "/login.html";
+    }
+
+}
---
+++ src/main/java/group8/skyweaver_inventory/models/User.java
@@ -0,0 +1,47 @@
+package group8.skyweaver_inventory.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String username;
+    private String password;
+    private String Id; // either Manager or Employee
+
+    public User() {
+    }
+
+    public User(String username, String password, String Id) {
+        this.username = username;
+        this.password = password;
+        this.Id = Id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+}
