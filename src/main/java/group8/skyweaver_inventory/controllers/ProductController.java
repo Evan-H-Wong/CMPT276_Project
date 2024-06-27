f3dc5aa57c7360dc1ab18a19395c528c4f9703b9
unknown <ehw4@sfu.ca>
2024-06-27 14:57:03 -0700

    feat: created template headers and footers

--- src/main/java/group8/skyweaver_inventory/controllers/ProductController.java
+++ src/main/java/group8/skyweaver_inventory/controllers/ProductController.java
@@ -2,6 +2,7 @@ package group8.skyweaver_inventory.controllers;
 
 import group8.skyweaver_inventory.models.Product;
 import group8.skyweaver_inventory.models.ProductRepository;
+import group8.skyweaver_inventory.models.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -17,6 +18,12 @@ import java.util.Map;
 @Controller
 public class ProductController {
     
-    
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/managestock")
+    public String redirect() {
+        return "manager/managestock.html";
+    }
 
 }
