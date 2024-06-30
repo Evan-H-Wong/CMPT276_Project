fd2b7d6f8e071fc93d0c696fcb9ced420146ecc1
unknown <ehw4@sfu.ca>
2024-06-29 18:38:22 -0700

    feat: added more visibility features to managestock; edit: modified prices to be fixed to 2 decimal places

--- src/main/java/group8/skyweaver_inventory/controllers/ProductController.java
+++ src/main/java/group8/skyweaver_inventory/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Map;
 
@@ -54,13 +55,24 @@ public class ProductController {
     @GetMapping("/managestock")
     public String stockRedirect(Model model) {
         List<Product> products = productRepository.findByOrderByProductNameAsc();
-        model.addAttribute("p",products);
+        List<Product> outofstock = products.stream().filter(obj->obj.getProductQuantity() == 0).collect(Collectors.toList());
+        List<Product> lowstock = products.stream().filter(obj->obj.getProductQuantity() < 12).collect(Collectors.toList());
+        model.addAttribute("p", products);
+        model.addAttribute("rowCount", products.size());
+        model.addAttribute("outofstock", outofstock.size());
+        model.addAttribute("lowstock", lowstock.size());
         return "manager/managestock.html";
     }
 
     @GetMapping("/editproduct")
     public String editStock(Model model, @RequestParam Map<String, String> ToEdit) {
+        List<Product> products = productRepository.findByOrderByProductNameAsc();
+        List<Product> outofstock = products.stream().filter(obj->obj.getProductQuantity() == 0).collect(Collectors.toList());
+        List<Product> lowstock = products.stream().filter(obj->obj.getProductQuantity() < 12).collect(Collectors.toList());
         Product product = productRepository.findByPid(Integer.parseInt(ToEdit.get("toEdit")));
+        model.addAttribute("rowCount", products.size());
+        model.addAttribute("outofstock", outofstock.size());
+        model.addAttribute("lowstock", lowstock.size());
         model.addAttribute("p", product);
         return "manager/editproduct.html";
     }
@@ -71,7 +83,8 @@ public class ProductController {
         Product modProduct = productRepository.findById(Integer.parseInt(ToApply.get("pid"))).get();
         modProduct.setProductName(ToApply.get("name"));
         modProduct.setProductCategory(ToApply.get("category"));
-        modProduct.setProductPrice(Float.parseFloat(ToApply.get("price")));
+        String temp = String.format("%.2f", Float.parseFloat(ToApply.get("price")));
+        modProduct.setProductPrice(Float.parseFloat(temp));
         modProduct.setProductQuantity(Integer.parseInt(ToApply.get("quantity")));
         productRepository.save(modProduct);
         return "redirect:/managestock";
