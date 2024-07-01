97d1b859ca31ad5030b3d3cdc64f3a55358900fd
unknown <ehw4@sfu.ca>
2024-07-01 12:49:48 -0700

    edit: Made homepage show db data

--- src/main/java/group8/skyweaver_inventory/controllers/ProductController.java
+++ src/main/java/group8/skyweaver_inventory/controllers/ProductController.java
@@ -14,9 +14,6 @@ import org.springframework.web.servlet.view.RedirectView;
 import java.util.stream.Collectors;
 import java.util.List;
 import java.util.Map;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class ProductController {
@@ -92,6 +89,5 @@ public class ProductController {
         productRepository.save(modProduct);
         return "redirect:/managestock";
     }
-    
 
 }
